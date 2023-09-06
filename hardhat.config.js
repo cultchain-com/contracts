827cd8c18f36a79ba5bd7ec5cd4f3b26ce94974e
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-09-06 16:10:48 +0300

    PK hardcoded due to the dotenv problem, TODO fixed later

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,12 +1,28 @@
-/* hardhat.config.js */
+const dotenv = require("dotenv");
 require("@nomiclabs/hardhat-waffle");
+dotenv.config({ path: __dirname + "/.env" });
 privateKey = process.env.TEST_PRIVATE_KEY;
+testNetPrivateKey = process.env.TEST_NET_PRIVATE_KEY;
+secNetPk = process.env.SEC_NET_PK;
+console.log(testNetPrivateKey);
+let pk = "8eb2e13f92e850fb487aa6ff5aa786818d440395115ba91baf34e33d6722ac24";
 
 module.exports = {
   defaultNetwork: "hardhat",
   networks: {
     hardhat: {
       chainId: 1337,
+    },
+    mumbai: {
+      url: "https://lb.drpc.org/ogrpc?network=polygon-mumbai&dkey=AnfIhgajAkObjQxDb49BcekF0oq3S8YR7pVkdk2eQLM2",
+      accounts: [pk],
+      gas: 2100000,
+      gasPrice: 8000000000,
+      saveDeployments: true,
+    },
+    mainnet: {
+      url: "https://lb.drpc.org/ogrpc?network=polygon&dkey=AnfIhgajAkObjQxDb49BcekF0oq3S8YR7pVkdk2eQLM2",
+      accounts: [pk],
     },
   },
   solidity: {
@@ -17,5 +33,11 @@ module.exports = {
         runs: 200,
       },
     },
+  },
+  paths: {
+    sources: "./contracts",
+    tests: "./test",
+    cache: "./cache",
+    artifacts: "./artifacts",
   },
 };
