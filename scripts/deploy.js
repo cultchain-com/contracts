8a940d86f05cabf6e4086dca76b5822d5acf4732
Reza Jahankohan <reza.jahankohan@pixiongames.com>
2023-09-16 02:33:54 +0300

    Deploy script updated with the new contract deployements

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,8 +14,34 @@ async function main() {
   const charityEvent = await CharityEvent.deploy(randomizedCommittee.address);
 
   await charityEvent.deployed();
-
   console.log("CharityEvent deployed to:", charityEvent.address);
+
+  await randomizedCommittee.grantValidatorRole(charityEvent.address);
+  console.log("Validator Role granted to charity contract");
+
+  const Validator = await hre.ethers.getContractFactory("Validator");
+  const validator = await Validator.deploy(randomizedCommittee.address);
+
+  await validator.deployed();
+  console.log("Validator Contract deployed at:", validator.address);
+
+  const Fundraising = await hre.ethers.getContractFactory("Fundraising");
+  const fundraising = await Fundraising.deploy(
+    charityEvent.address,
+    randomizedCommittee.address,
+    "0x3da2581ad70D98a45ebE7D3A51D821ddDe640432"
+  );
+
+  await fundraising.deployed();
+  console.log("Fundraising contract deployed at:", fundraising.address);
+
+  await randomizedCommittee.updateValidatorCharityEventAddress(
+    validator.address,
+    charityEvent.address
+  );
+  console.log(
+    "Validator and Charity Event contract address updated at Randomized Committee"
+  );
 }
 
 main()
